b1440c45a4501fd234661ac5252c05fd908cb52c
Kareem sakkary <kareemsakkary101@gmail.com>
2022-10-31 22:50:59 +0200

    Initial commit

---
+++ BankAccount.cpp
@@ -0,0 +1,4 @@
+//
+// Created by karee on 31/10/2022.
+//
+
---
+++ classes.h
@@ -0,0 +1,8 @@
+//
+// Created by karee on 31/10/2022.
+//
+
+#ifndef BANKSYSTEM_CLASSES_H
+#define BANKSYSTEM_CLASSES_H
+
+#endif //BANKSYSTEM_CLASSES_H
